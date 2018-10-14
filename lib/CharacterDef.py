f9836b5e53425b12c3299444d41bcdb5cb45fc18
Ben <bendaz@users.noreply.github.com>
2018-10-14 00:16:09 -0500

    Deleted characterDef file

--- lib/CharacterDef.py
+++
@@ -1,6 +0,0 @@
-commonRaces = [dwarf, elf, gnome, halfElf, halfOrc, halfling, human]
-rareRaces = [antipaladin]
-goblinoidRaces = [goblin, orc, hobgoblin]
-
-commonClasses = [Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Wizard]
-advancedClasses = [Alchemist, Cavalier, Inquisitor, Oracle, Summoner, Witch]
