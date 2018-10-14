9b357ee9d84ef76f05540ba757429108f5d2c1a3
Ben <bendaz@users.noreply.github.com>
2018-10-14 00:16:31 -0500

    Started shop radomization routine

--- lib/Shop.py
+++ lib/Shop.py
@@ -1,9 +1,44 @@
+import Item
+
 class Shop:
+	randomize = None
 	city = None
 	name = None
 	owner = None
 	shopType = None
 	inventory = None
 
-	__init__(self, name):
+	def __init__(self, randomize, city = None, name = None, owner = None, shopType = None, inventory = None):
+		self.city = city
 		self.name = name
+		self.owner = owner
+		self.shopType = shopType
+		self.inventory = inventory
+
+		if (randomize.lower() == 'y'):
+			if (city == None):
+				pass
+			if (name == None):
+				pass
+			if (owner == None):
+				pass
+			if (shopType == None):
+				self.shopType == random.choose(shopTypes)
+			if (inventory == None):
+				if (shopType == 'general'):
+					self.inventory = Item.getItemsOfType('gear')
+				elif (shopType == 'blacksmith'):
+					self.inventory = Item.getItemsOfType('weapons') + Item.getItemsOfType('armor')
+				elif (shopType == 'stable'):
+					self.inventory = Item.getItemsOfType('stable')
+				elif (shopType == 'armorer'):
+					self.inventory = Item.getItemsOfType('armor')
+				elif (shopType == 'weaponsmith'):
+					self.inventory = Item.getItemsOfType('weapons')
+				elif (shopType == 'dock'):
+					self.inventory = Item.getItemsOfType('boat')
+				else:
+					self.inventory = getAllItems()
+		return
+
+shopTypes = ['general', 'blacksmith', 'stable', 'armorer', 'weaponsmith', 'dock']
