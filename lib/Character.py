ff1ee6f974c267f8f95943e3fb8e442b817848b4
Ben <bendaz@users.noreply.github.com>
2018-10-14 00:15:57 -0500

    Added some defs to the file for randomization

--- lib/Character.py
+++ lib/Character.py
@@ -234,3 +234,11 @@ class Character:
 			self.gender = inGender
 		else:
 			self.gender = random.choice(["M", "F"])
+
+commonRaces = [dwarf, elf, gnome, halfElf, halfOrc, halfling, human]
+rareRaces = [antipaladin]
+goblinoidRaces = [goblin, orc, hobgoblin]
+
+commonClasses = [Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Wizard]
+advancedClasses = [Alchemist, Cavalier, Inquisitor, Oracle, Summoner, Witch]
+
